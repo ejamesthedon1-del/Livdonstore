e0b2d9d2b4fad6f587995892035e5deeb9b3c01a
ejamesthedon1-del <ejamesthedon1@gmail.com>
2026-01-31 00:14:03 -0600

    Update hero section: single image, bold uppercase text, outline button under headline

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,49 +1,44 @@
 const HeroSection = () => {
-  const heroImages = [
-    {
-      id: 1,
-      image: 'https://images.unsplash.com/photo-1441986300917-64674bd600d8?ixlib=rb-4.0.3&auto=format&fit=crop&w=2070&q=80',
-      link: '#new-collection',
-    },
-    {
-      id: 2,
-      image: 'https://images.unsplash.com/photo-1445205170230-053b83016050?ixlib=rb-4.0.3&auto=format&fit=crop&w=2071&q=80',
-      link: '#featured',
-    },
-  ]
+  const heroImage = {
+    image: 'https://images.unsplash.com/photo-1441986300917-64674bd600d8?ixlib=rb-4.0.3&auto=format&fit=crop&w=2070&q=80',
+    link: '#new-collection',
+  }
 
   return (
     <div className="o-blocks o-blocks--40">
       <div className="o-tetris o-tetris--o small-mt">
-        {heroImages.map((item) => (
-          <div key={item.id} className="m-media m-media--has-media">
-            <div className="m-media__media a-ratio-box a-ratio-box--1:1" data-row-number="1" data-picture-number={item.id}>
-              <a href={item.link} className="m-media__media-cta">
-                <img
-                  src={item.image}
-                  alt=""
-                  title=""
-                  data-sizes="auto"
-                  className=""
-                />
-              </a>
-            </div>
+        <div className="m-media m-media--has-media">
+          <div className="m-media__media a-ratio-box a-ratio-box--1:1" data-row-number="1" data-picture-number="1">
+            <a href={heroImage.link} className="m-media__media-cta">
+              <img
+                src={heroImage.image}
+                alt=""
+                title=""
+                data-sizes="auto"
+                className=""
+              />
+            </a>
           </div>
-        ))}
+        </div>
       </div>
       
       <div className="a-rich-text a-rich-text--main title-and-link-and-desc">
-        <h1 className="f-display--02">
+        <h1 className="f-display--02 f-display--bold">
           NEW COLLECTION "PRINTEMPS 2026"
         </h1>
         <div className="a-rich-text__inner">
           <div className="m-media m-media--has-text">
             <div className="m-media__text">
-              <p className="f-body">
-                Discover the new arrivals for Spring.
+              <p className="f-body f-body--bold">
+                DISCOVER THE NEW ARRIVALS FOR SPRING.
               </p>
             </div>
           </div>
+        </div>
+        <div className="a-rich-text__button">
+          <a href={heroImage.link} className="a-btn a-btn--outline">
+            DISCOVER OUR NEW COLLECTION
+          </a>
         </div>
       </div>
     </div>
